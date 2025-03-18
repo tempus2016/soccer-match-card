db2806e6017e41eb923f05e129debea83998b599
tempus2016 <40920327+tempus2016@users.noreply.github.com>
2025-03-18 19:56:48 +0100

    Update soccer-match-card.js

--- soccer-match-card.js
+++ soccer-match-card.js
@@ -115,13 +115,14 @@ class SoccerMatchCard extends HTMLElement {
     }
   }
 
-  render() {
-    this.shadowRoot.innerHTML = `
-      <ha-card>
-        <div style="color: white; padding: 16px;">⚽️ Hello World! Render is working!</div>
-      </ha-card>
-    `;
-  }
+render() {
+  this.shadowRoot.innerHTML = `
+    <ha-card>
+      <div style="color:white; padding: 16px;">✅ This is working!</div>
+    </ha-card>
+  `;
+}
+
   
 
   setStyle() {
