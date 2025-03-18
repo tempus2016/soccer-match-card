d210c255e52cc9d7546511594cf8854926356217
tempus2016 <40920327+tempus2016@users.noreply.github.com>
2025-03-18 22:23:53 +0100

    Update soccer-match-card.js

--- soccer-match-card.js
+++ soccer-match-card.js
@@ -9,18 +9,35 @@ class SoccerMatchCard extends HTMLElement {
     this.previousStateObj = null;
   }
 
+  // The user supplied configuration. Throw an exception and Home Assistant
+  // will render an error card.
   setConfig(config) {
     if (!config.entity) {
-      throw new Error('You need to define an entity');
+      throw new Error("You need to define an entity");
     }
-
     this.config = config;
-    this.render();
   }
 
   set hass(hass) {
-    this._hass = hass;
-    if (!this.config) return;
+    // Initialize the content if it's not there yet.
+    if (!this.content) {
+      this.innerHTML = `
+        <ha-card header="Example-card">
+          <div class="card-content"></div>
+        </ha-card>
+      `;
+      this.content = this.querySelector("div");
+    }
+
+    const entityId = this.config.entity;
+    const state = hass.states[entityId];
+    const stateStr = state ? state.state : "unavailable";
+
+    this.content.innerHTML = `
+      The state of ${entityId} is ${stateStr}!
+      <br><br>
+      <img src="http://via.placeholder.com/350x150">
+    `;
 
     const entityId = this.config.entity;
     if (!entityId) return;
